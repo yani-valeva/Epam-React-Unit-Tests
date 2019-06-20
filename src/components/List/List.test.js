e327b710f046a29bf1429f912285559a08310326
yani-valeva <yani.valeva@gmail.com>
2019-06-20 09:35:15 +0300

    Fixed List test and delete unnesessary ListItem imports

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -11,13 +11,13 @@ describe("<List />", () => {
             expect(shallow(<List />).contains(<div> No items found.</div>)).toBe(true);
         });
         test("what is visible", () => {
-            const mockedItems = {
+            const mockedItems = [{
                 "albumId": 1,
                 "id": 1,
                 "title": "accusamus beatae ad facilis cum similique qui sunt",
                 "url": "https://via.placeholder.com/600/92c952",
                 "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-            };
+            }];
 
             const wrapper = mount(<List items={mockedItems} />);
             expect(wrapper.find(".listItem__title").length === 1).toBe(true);
